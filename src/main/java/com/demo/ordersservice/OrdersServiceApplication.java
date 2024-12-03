ab0103a2b751a893b2635b0f79bcfe50a45a1407
Steve Womack <stwomack@gmail.com>
2024-12-03 16:22:04 -0600

    Trying yet another config name

--- src/main/java/com/demo/ordersservice/OrdersServiceApplication.java
+++ src/main/java/com/demo/ordersservice/OrdersServiceApplication.java
@@ -1,10 +1,7 @@
 package com.demo.ordersservice;
 
-import io.micrometer.tracing.Span;
-import io.micrometer.tracing.Tracer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -18,9 +15,6 @@ import java.util.Random;
 
 public class OrdersServiceApplication {
 	private final Logger LOG = LoggerFactory.getLogger(OrdersServiceApplication.class);
-	@Autowired
-	private Tracer tracer;
-
 	public static void main(String[] args) {
 		SpringApplication.run(OrdersServiceApplication.class, args);
 	}
@@ -33,14 +27,10 @@ public class OrdersServiceApplication {
 		RestTemplate restTemplate = new RestTemplate();
         LOG.info("Created new order: {}", customOrder.toString());
 		String response = "No Response";
-		Span spanBulder = tracer.spanBuilder().name("orders-service").remoteIpAndPort("http://grafana-k8s-monitoring-alloy.default.svc.cluster.local", 4318).start();
 		try {
-			LOG.info("Span: {}", spanBulder.toString());
 			response = restTemplate.postForObject("http://shipping-service/shipping", customOrder, String.class);
 		} catch (Exception e) {
 			LOG.error("Ooops, shipping service probably down: {}", e.getMessage());
-		} finally {
-			spanBulder.end();
 		}
 
         LOG.info("Response {}", response);
@@ -56,4 +46,5 @@ public class OrdersServiceApplication {
 
 		return customOrder;
 	}
+
 }
