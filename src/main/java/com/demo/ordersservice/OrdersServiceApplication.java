a8a63f6396818c159dcbd31066658a5d86f317d2
Steve Womack <stwomack@gmail.com>
2024-12-13 19:50:52 -0600

    Working

--- src/main/java/com/demo/ordersservice/OrdersServiceApplication.java
+++ src/main/java/com/demo/ordersservice/OrdersServiceApplication.java
@@ -1,50 +1,57 @@
 package com.demo.ordersservice;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
-
-import org.springframework.web.client.RestTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
 
 import java.util.Random;
+
 @SpringBootApplication
 @EnableScheduling
-
 public class OrdersServiceApplication {
-	private final Logger LOG = LoggerFactory.getLogger(OrdersServiceApplication.class);
-	public static void main(String[] args) {
-		SpringApplication.run(OrdersServiceApplication.class, args);
-	}
+    private final Logger LOG = LoggerFactory.getLogger(OrdersServiceApplication.class);
 
-	@Scheduled(fixedRate
-			= 5000)
-	public void generateOrder() {
-		CustomOrder customOrder = generateRandomOrder();
-		System.out.println(customOrder.toString());
-		RestTemplate restTemplate = new RestTemplate();
+    private final OrderRepository orderRepository;
+
+    public OrdersServiceApplication(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrdersServiceApplication.class, args);
+    }
+
+    @Scheduled(fixedRate = 5000)
+    public void generateOrder() {
+        CustomOrder customOrder = generateRandomOrder();
+        LOG.info(customOrder.toString());
         LOG.info("Created new order: {}", customOrder.toString());
-		String response = "No Response";
-		try {
-			response = restTemplate.postForObject("http://shipping-service/shipping", customOrder, String.class);
-		} catch (Exception e) {
-			LOG.error("Ooops, shipping service probably down: {}", e.getMessage());
-		}
+        String response = "No Response";
+        try {
+            orderRepository.save(customOrder);
+        } catch (Exception e) {
+            LOG.error("Ooops: {}", e.getMessage());
+        }
 
         LOG.info("Response {}", response);
-	}
+    }
 
-	public static CustomOrder generateRandomOrder() {
-		Random random = new Random();
+    public static CustomOrder generateRandomOrder() {
+        Random random = new Random();
 
-		CustomOrder customOrder = new CustomOrder();
-		customOrder.setCustomerName("Customer " + random.nextInt(1000));
-		customOrder.setItemName("Item: " + random.nextInt(1000));
-		customOrder.setQuantity(+ random.nextInt(20));
+        CustomOrder customOrder = new CustomOrder();
+        customOrder.setCustomerName("Customer " + random.nextInt(1000));
+        customOrder.setItemName("Item: " + random.nextInt(1000));
+        customOrder.setQuantity(random.nextInt(20));
 
-		return customOrder;
-	}
-
+        return customOrder;
+    }
+}
+@Repository
+interface OrderRepository extends MongoRepository<CustomOrder, Long> {
 }
